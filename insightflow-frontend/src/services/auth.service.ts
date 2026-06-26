ef069a0dde584055cc1f00f48a38fbca7149fedd
jemaeee17 <jemaegracezamora@gmail.com>
2026-06-26 22:57:38 +0800

    Use shared API client for auth service

--- insightflow-frontend/src/services/auth.service.ts
+++ insightflow-frontend/src/services/auth.service.ts
@@ -1,14 +1,6 @@
-import axios from "axios";
-
-const api = axios.create({
-    baseURL: "http://localhost:8000/api",
-    headers: {
-        Accept: "application/json",
-    },
-});
+import { api } from "../lib/api";
 
 export const authService = {
-
     async login(email: string, password: string) {
         const { data } = await api.post("/login", {
             email,
